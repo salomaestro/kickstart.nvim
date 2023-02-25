931f5e9107623e439d10ae7b6633054de14f6a04
salomaestro <chris10an.salomonsen@gmail.com>
2023-02-25 13:56:52 +0100

    Ported keybinds from previous config + some new ones

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,46 @@
+vim.opt.relativenumber = true
+
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Move visual selected lines with indentation (wow).
+vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv")
+
+-- Concatenates line below cursor but does not move cursor
+vim.keymap.set('n', 'J', "mzJ`z")
+
+-- Center cursor when hopping half page.
+vim.keymap.set('n', '<C-d>', "<C-d>zz")
+vim.keymap.set('n', '<C-u>', "<C-u>zz")
+
+-- Center cursor when looking through find
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', 'n', 'nzzzv')
+
+-- Store what was yanked in a void buffer (can therefore put multiple times)
+vim.keymap.set('x', '<leader>p', "\"_dP")
+
+-- Open useful telescope help windows.
+vim.keymap.set('n', '<leader>th', ":Telescope help_tags <cr>")
+vim.keymap.set('n', '<leader>tk', ":Telescope keymaps <cr>")
+
+vim.keymap.set('n', '<C-j>', "<cmd>cprev<cr>zz")
+vim.keymap.set('n', '<C-k>', "<cmd>cnext<cr>zz")
+vim.keymap.set('n', '<leader>j', "<cmd>lprev<cr>zz")
+vim.keymap.set('n', '<leader>k', "<cmd>lnext<cr>zz")
+
+vim.g.table_mode_corners = '|'
+vim.keymap.set('n', '<leader>mp', ":MarkdownPreview <cr>")
+local t_opts = { silent = true }
+-- vim.keymap.set('n', '<leader>tn', ":FloatermToggle <cr>")
+vim.keymap.set('t', '<esc>', '<C-\\><C-N>', t_opts)
+
+-- Python reorder imports command
+local fname = vim.api.nvim_buf_get_name(0)
+vim.keymap.set('n', '<leader>ri', ":!reorder-python-imports " .. fname .. "<cr><cr>")
+
+-- Tab keybinds
+vim.keymap.set('n', '<leader>te', ':tabnew<cr>:Explore<cr>')
+vim.keymap.set('n', '<leader>tn', ':tabnew<cr>')
---
+++ after/plugin/neotree.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', '§', ':Neotree reveal toggle<cr>')
+vim.keymap.set('n', '<leader>bf', ':Neotree toggle current reveal_force_cwd<cr>')
+vim.keymap.set('n', '<leader>bt', ':Neotree toggle show buffers right<cr>')
+vim.keymap.set('n', '<leader>bg', ':Neotree float git_status<cr>')
+
