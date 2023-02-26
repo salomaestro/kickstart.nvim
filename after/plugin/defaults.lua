2cfbd8583377dc4079f889180b9c8e8917247c4b
salomaestro <chris10an.salomonsen@gmail.com>
2023-02-26 14:59:55 +0100

    enhancing the default keybinds

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,5 @@
+local opts = { silent = true }
+
 vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
@@ -26,13 +28,14 @@ vim.keymap.set('x', '<leader>p', "\"_dP")
 vim.keymap.set('n', '<leader>th', ":Telescope help_tags <cr>")
 vim.keymap.set('n', '<leader>tk', ":Telescope keymaps <cr>")
 
-vim.keymap.set('n', '<C-j>', "<cmd>cprev<cr>zz")
-vim.keymap.set('n', '<C-k>', "<cmd>cnext<cr>zz")
+-- vim.keymap.set('n', '<C-j>', "<cmd>cprev<cr>zz")
+-- vim.keymap.set('n', '<C-k>', "<cmd>cnext<cr>zz")
 vim.keymap.set('n', '<leader>j', "<cmd>lprev<cr>zz")
 vim.keymap.set('n', '<leader>k', "<cmd>lnext<cr>zz")
 
 vim.g.table_mode_corners = '|'
 vim.keymap.set('n', '<leader>mp', ":MarkdownPreview <cr>")
+
 local t_opts = { silent = true }
 -- vim.keymap.set('n', '<leader>tn', ":FloatermToggle <cr>")
 vim.keymap.set('t', '<esc>', '<C-\\><C-N>', t_opts)
@@ -44,3 +47,17 @@ vim.keymap.set('n', '<leader>ri', ":!reorder-python-imports " .. fname .. "<cr><
 -- Tab keybinds
 vim.keymap.set('n', '<leader>te', ':tabnew<cr>:Explore<cr>')
 vim.keymap.set('n', '<leader>tn', ':tabnew<cr>')
+
+-- Easier window navigation
+vim.keymap.set('n', '<c-h>', '<c-w>h', opts)
+vim.keymap.set('n', '<c-j>', '<c-w>j', opts)
+vim.keymap.set('n', '<c-k>', '<c-w>k', opts)
+vim.keymap.set('n', '<c-l>', '<c-w>l', opts)
+
+-- Naviagate buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Insert newline without moving cursor and is repeatable
+vim.keymap.set('n', '<leader>o', ':<c-u>call append(line("."),   repeat([""], v:count1))<CR>', opts)
+vim.keymap.set('n', '<leader>O', ':<c-u>call append(line(".")-1,   repeat([""], v:count1))<CR>', opts)
