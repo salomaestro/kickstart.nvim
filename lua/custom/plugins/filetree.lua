786b772e5cea3fc640f643d89640ae1f7d53f676
salomaestro <chris10an.salomonsen@gmail.com>
2023-02-26 14:59:08 +0100

    trying to get symbols working with filetree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,6 +10,30 @@ return {
     -- Unless you are still migrating, remove the deprecated commands from v1.x
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      default_component_configs = {
+        git_status = {
+          symbols = {
+              -- Change type
+              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted   = "✖",-- this can only be used in the git_status source
+              renamed   = "",-- this can only be used in the git_status source
+              -- Status type
+              untracked = "",
+              ignored   = "",
+              unstaged  = "",
+              staged    = "",
+              conflict  = "",
+            }
+        },
+        indent = {
+          with_markers = true,
+          indent_marker = "│",
+          last_indent_marker = "└",
+          indent_size = 2,
+        },
+      }
+    }
   end,
 }
