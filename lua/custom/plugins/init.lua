65100d26210bfb6850b4a4fbd0e61a82177c01f7
salomaestro <chris10an.salomonsen@gmail.com>
2023-03-25 00:24:29 +0100

    dashboard moved to separate file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -35,6 +35,7 @@ return {
     config = true,
     cmd = "Glow",
   },
+
   -- {
   --   'ms-jpq/coq_nvim',
   --   branch = 'coq',
@@ -56,31 +57,5 @@ return {
         show_end_of_line = true,
       }
     end,
-  },
-  {
-    'glepnir/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        -- config
-        theme = 'hyper', --  theme is doom and hyper default is hyper
-        disable_move = false, --  defualt is false disable move keymap for hyper
-        shortcut_type = 'number', --  shorcut type 'letter' or 'number'
-        change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
-        config = {}, --  config used for theme
-        hide = {
-          statusline = false, -- hide statusline default is true
-          tabline = false, -- hide the tabline
-          winbar = false, -- hide winbar
-        },
-        preview = {
-          -- command = true, -- preview command
-          file_path = true, -- preview file path
-          file_height = true, -- preview file height
-          file_width = true, -- preview file width
-        },
-      }
-    end,
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
   },
 }
