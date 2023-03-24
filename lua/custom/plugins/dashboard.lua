d569ccd4689c676ae493ee46648621a07afe7314
salomaestro <chris10an.salomonsen@gmail.com>
2023-03-25 00:23:10 +0100

    add dashboard to separate file and add [g] for git

---
+++ lua/custom/plugins/dashboard.lua
@@ -0,0 +1,51 @@
+return {
+    {
+        'glepnir/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup {
+                theme = 'hyper',
+                config = {
+                    week_header = {
+                        enable = true,
+                    },
+                    shortcut = {
+                        {
+                            desc = ' Update',
+                            group = '@property',
+                            action = 'Lazy update',
+                            key = 'u'
+                        },
+                        {
+                            icon = ' ',
+                            icon_hl = '@variable',
+                            desc = 'Files',
+                            group = 'Label',
+                            action = 'Telescope find_files',
+                            key = 'f',
+                        },
+                        {
+                            desc = '⇅ Git',
+                            group = 'Number',
+                            action = 'Git',
+                            key = 'g',
+                        }
+                        -- {
+                        --     desc = ' Apps',
+                        --     group = 'DiagnosticHint',
+                        --     action = 'Telescope app',
+                        --     key = 'a',
+                        -- },
+                        -- {
+                        --     desc = ' dotfiles',
+                        --     group = 'Number',
+                        --     action = 'Telescope dotfiles',
+                        --     key = 'd',
+                        -- },
+                    },
+                },
+            }
+        end,
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+}
