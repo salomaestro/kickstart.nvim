1dfa91aaccf78b55e87530a86771a1b0482ba16a
salomaestro <chris10an.salomonsen@gmail.com>
2023-03-25 01:40:59 +0100

    switch to evil lualine, but with transparent bg (bg col = nil)

--- init.lua
+++ init.lua
@@ -79,12 +79,12 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      {'williamboman/mason.nvim', config = true},
+      { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -126,14 +126,14 @@ require('lazy').setup({
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
+    -- opts = {
+    --   options = {
+    -- icons_enabled = true,
+    -- theme = 'onedark',
+    -- component_separators = '|',
+    -- section_separators = '',
+    --   },
+    -- },
   },
 
   {
